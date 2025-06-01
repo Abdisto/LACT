c874f50fc07ff700cd66f48362d87e552148b092
Abdist <nasiefabdullah@gmail.com>
2025-06-01 23:03:33 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -37,10 +37,16 @@
           gtk4
           libdrm
           vulkan-loader
+          vulkan-tools
           hwdata
           coreutils
           ocl-icd
           fuse3
+          autoAddDriverRunpath
+          nixosTests
+          nix-update-script
+          systemdMinimal
+          lib
         ];
 
         checkFlags = [
@@ -50,18 +56,31 @@
 
         postPatch = ''
           substituteInPlace lact-daemon/src/server/system.rs \
-            --replace-fail 'Command::new("uname")' 'Command::new("${pkgs.coreutils}/bin/uname")'
-
+            --replace-fail 'Command::new("uname")' 'Command::new("${coreutils}/bin/uname")'
+          substituteInPlace lact-daemon/src/server/profiles.rs \
+            --replace-fail 'Command::new("uname")' 'Command::new("${coreutils}/bin/uname")'
+      
+          substituteInPlace lact-daemon/src/server/handler.rs \
+            --replace-fail 'Command::new("journalctl")' 'Command::new("${systemdMinimal}/bin/journalctl")'
+      
+          substituteInPlace lact-daemon/src/server/vulkan.rs \
+            --replace-fail 'Command::new("vulkaninfo")' 'Command::new("${vulkan-tools}/bin/vulkaninfo")'
+      
           substituteInPlace res/lactd.service \
             --replace-fail ExecStart={lact,$out/bin/lact}
-
+      
           substituteInPlace res/io.github.ilya_zlobintsev.LACT.desktop \
             --replace-fail Exec={lact,$out/bin/lact}
+      
+          # read() looks for the database in /usr/share so we use read_from_file() instead
+          substituteInPlace lact-daemon/src/server/handler.rs \
+            --replace-fail 'Database::read()' 'Database::read_from_file("${hwdata}/share/hwdata/pci.ids")'
         '';
 
         postInstall = ''
           install -Dm444 res/lactd.service -t $out/lib/systemd/system
           install -Dm444 res/io.github.ilya_zlobintsev.LACT.desktop -t $out/share/applications
+          install -Dm444 res/io.github.ilya_zlobintsev.LACT.svg -t $out/share/pixmaps
         '';
 
         postFixup = pkgs.lib.optionalString pkgs.stdenv.targetPlatform.isElf ''
@@ -75,6 +94,8 @@
           homepage = "https://github.com/ilya-zlobintsev/LACT";
           license = pkgs.lib.licenses.mit;
           maintainers = with pkgs.lib.maintainers; [
+            cything
+            figsoda
             figsoda
             atemu
           ];
